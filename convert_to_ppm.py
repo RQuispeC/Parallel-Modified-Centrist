1978333a691aa071c89f031da77422a7371d4919
RQuispeC <quispe@liv.ic.unicamp.br>
2017-05-29 10:55:40 -0300

    Modification to convert all files in dataset to .ppm

--- convert_to_ppm.py
+++ convert_to_ppm.py
@@ -1,9 +1,25 @@
 import cv2 #opencv must be installed
-#name of the input image
-img_input_name = "images/tmp.jpg"
-#name of the ouput image
-img_out_name = "images/tmp.ppm"
+import os
 
-img = cv2.imread(img_input_name)
-cv2.imwrite(img_out_name, img)
-print "Done ... out file in", img_out_name 
+def convertImage(imageName):
+  #name of the input image
+  img_input_name = imageName
+  #name of the ouput image
+  img_out_name = imageName[:imageName.rfind('.')]+ ".ppm"
+
+  img = cv2.imread(img_input_name)
+  cv2.imwrite(img_out_name, img)
+  print "Done ... out file in", img_out_name
+
+def convertORL_faces(filePath):
+  trainingClasses = os.listdir(filePath)
+  trainingClasses.sort()
+  for person in trainingClasses:
+    imageNames = os.listdir(filePath + '/' + person)
+    print '\t', filePath + '/' + person
+    for imageName in imageNames:
+      print '\t', filePath + '/' + person + '/' + imageName
+      convertImage(filePath + '/' + person + '/' + imageName)
+      os.remove(filePath + '/' + person + '/' + imageName)
+
+convertORL_faces('orl_faces')
